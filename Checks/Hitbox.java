c8b029b2471c9b6a3fc4d4b5662d2d670eb44671
koloslolya <59935145+SleepyKolosLolya@users.noreply.github.com>
2020-10-08 21:35:53 +0300

    Update Hitbox.java

--- Checks/Hitbox.java
+++ Checks/Hitbox.java
@@ -1,14 +1,19 @@
 
-public class HitBoxA implements Listener {
+public class HitBoxA extends Checl {
 
-    @EventHandler
-    public static void onDamage(@NotNull EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player) {
-            KacPlayer kacPlayer = KacPlayerManager.getPlayer((Player) event.getDamager());
+    public HitBoxA(Data data) {
+        super(data, CheckType.ACCESS, "HitBox (A)"
+    }
+    
+    @Override
+    public void handle(@NotNull Event e) {
+        if (e instanceof UseEntity) {
+            UseEntity event = (UseEntity) e;
+            Data data = DataManager.getPlayer((Player) event.getDamager());
             Entity entity = event.getEntity();
-            double lookingAt = isLookingAt(kacPlayer.user, entity.getLocation());
+            double lookingAt = isLookingAt(data.user, entity.getLocation());
             double hitbox_rate = .34;
-            double distance = kacPlayer.user.getLocation().distance(entity.getLocation());
+            double distance = data.user.getLocation().distance(entity.getLocation());
             if (distance < 0.7) {
                 hitbox_rate = .1;
             } else if (distance > 4.3) {
@@ -22,20 +27,12 @@ public class HitBoxA implements Listener {
             }
             if (distance > 0.5 && lookingAt < hitbox_rate) {
                 // Flag system
-                Bukkit.getServer().getPluginManager().callEvent(new CheckResult(
-                        CheckType.HITBOX,
-                        "HitBox (A)",
-                        SetBackType.NONE,
-                        true,
-                        String.format(" { lookingAt: %s; dy: %s; } ", lookingAt, distance),
-                        kacPlayer,
-                        1)
-                );
+                flag(String.format(" { lookingAt: %s; dy: %s; } ", lookingAt, distance));
             }
         }
     }
 
-    private static double isLookingAt(@NotNull Player player, @NotNull Location target) {
+    private double isLookingAt(@NotNull Player player, @NotNull Location target) {
         Location eye = player.getEyeLocation();
         Vector toEntity = target.toVector().subtract(eye.toVector());
         return toEntity.normalize().dot(eye.getDirection());
